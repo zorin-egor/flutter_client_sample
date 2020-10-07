690624a0ddcb3f67c337135abffcb56bcd952fe6
zorin-egor <zorin.egor.apps@gmail.com>
2020-10-07 15:26:53 +0300

    Add check for request

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:flutterclientsample/data/details.dart';
 import 'package:flutterclientsample/data/user.dart';
 import 'package:http/http.dart' as Http;
-import 'package:synchronized/extension.dart';
 
 
 class Api {
@@ -16,52 +15,69 @@ class Api {
   Http.Response _response;
   String _url;
   String _userId;
+  bool _isUsersFuture;
+  bool _isDetailsFuture;
+
 
   Api([this._url]) {
     _url ??= BASE_URL;
     _userId = USERS_DEFAULT_ID;
+    _isUsersFuture = false;
+    _isDetailsFuture = false;
   }
 
   Future<List<User>> getUsersJson({ String sinceUser, error(message) }) async {
-    return await synchronized(() async {
-      try {
-        final userId = sinceUser ?? _userId;
-        _response = await Http.get(_url + USERS_PATH + userId);
-
-        if (200 <= _response.statusCode && _response.statusCode < 300) {
-          final users = User.fromJson(_response.body);
-          _userId = users.last.id;
-          return Future.value(users);
-        }
-
-        throw HttpException(_response.reasonPhrase);
-
-      } catch (e) {
-        return Future.error(e);
+    try {
+      if (_isUsersFuture) {
+        return Future.error("Request is still running");
       }
-    });
+
+      _isUsersFuture = true;
+      final userId = sinceUser ?? _userId;
+      _response = await Http.get(_url + USERS_PATH + userId);
+
+      if (200 <= _response.statusCode && _response.statusCode < 300) {
+        final users = User.fromJson(_response.body);
+        _userId = users.last.id;
+        return Future.value(users);
+      }
+
+      throw HttpException(_response.reasonPhrase);
+
+    } catch (e) {
+      return Future.error(e);
+    } finally {
+      _isUsersFuture = false;
+    }
   }
+
+
 
   Future<List<User>> getUserDefault() async {
     return getUsersJson(sinceUser: USERS_DEFAULT_ID);
   }
 
   Future<Details> getDetailsJson(String detailsUrl, { error(message) }) async {
-    return await synchronized(() async {
-      try {
-        _response = await Http.get(detailsUrl);
-
-        if (200 <= _response.statusCode && _response.statusCode < 300) {
-          final details = Details.fromJson(_response.body);
-          return Future.value(details);
-        }
-
-        throw HttpException(_response.reasonPhrase);
-
-      } catch (e) {
-        return Future.error(e);
+    try {
+      if (_isDetailsFuture) {
+        return Future.error("Request is still running");
       }
-    });
+
+      _isDetailsFuture = true;
+      _response = await Http.get(detailsUrl);
+
+      if (200 <= _response.statusCode && _response.statusCode < 300) {
+        final details = Details.fromJson(_response.body);
+        return Future.value(details);
+      }
+
+      throw HttpException(_response.reasonPhrase);
+
+    } catch (e) {
+      return Future.error(e);
+    } finally {
+      _isDetailsFuture = false;
+    }
   }
 
 }
